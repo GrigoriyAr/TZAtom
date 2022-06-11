f71d393c1c07b3744cf7f771dcb75a0010007e35
Grigoriy Arkhipov <arhipov.grigo@yandex.ru>
2022-06-12 00:07:38 +0300

    7

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import uvicorn
+from fastapi import FastAPI, UploadFile, HTTPException, status
+app = FastAPI()
 
 
-import sqlite3
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-def create_db():
-    with sqlite3.connect("sqlite.db") as conn:
-        cur = conn.cursor()
-        cur.execute("""CREATE TABLE IF NOT EXISTS inbox (
-        code INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        moment DATETIME
-        )""")
+@app.post('/frames/')
+async def create_upload_files(files: list[UploadFile]):
+    print(len(files))
+    if len(files) > 15:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Нельзя больше 15 файлов")
+    for i in files:
+        if '.jpg' not in i.filename:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Нельзя формат кроме jpg")
+    return {"filenames": [file.filename for file in files]}
+
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
